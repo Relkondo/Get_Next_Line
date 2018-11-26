6ffe8420924b8a9c8a3c46294658cbdfc35f9fd6
Samuel CORON <scoron@e3r4p13.42.fr>
2018-11-26 14:39:41 +0100

    commit

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scoron <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/18 16:18:45 by scoron            #+#    #+#             */
+/*   Updated: 2018/11/25 14:19:05 by scoron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 8
+
+typedef	struct		s_chain
+{
+	struct s_chain	*next;
+	struct s_chain	*previous;
+	int				fd;
+	char			*res;
+}					t_chain;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
